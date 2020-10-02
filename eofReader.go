905723c9b934ef5079ea48293b7206fa9a0b0062
Tobias Salzmann <796084+Eun@users.noreply.github.com>
2020-10-02 13:20:48 +0200

    remove unused code

--- eofReader.go
+++
@@ -1,9 +0,0 @@
-package yaegi_template
-
-import "io"
-
-type eofReader struct{}
-
-func (eofReader) Read([]byte) (int, error) {
-	return 0, io.EOF
-}
--- eofWriter.go
+++
@@ -1,9 +0,0 @@
-package yaegi_template
-
-import "io"
-
-type eofWriter struct{}
-
-func (eofWriter) Write([]byte) (int, error) {
-	return 0, io.EOF
-}
